a9a2d80557eb8f1dd48a587123cb2a8ecc22c0c8
Bearcat <bwigal2@nastudents.org>
2023-09-20 20:02:12 -0400

    Create index.php

---
+++ sign-up/index.php
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCi2nCSIgG06MN5WoVPiCf0uWQWXh6KPJw",
+    authDomain: "signup-7856c.firebaseapp.com",
+    projectId: "signup-7856c",
+    storageBucket: "signup-7856c.appspot.com",
+    messagingSenderId: "257328329466",
+    appId: "1:257328329466:web:6b8b340084ad8deec14f43",
+    measurementId: "G-B1Q51TBM2R"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
